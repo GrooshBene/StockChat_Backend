1457e5092aa1894fa800221d643379dcd994e2e3
GrooshBene <wltn000129@gmail.com>
2017-05-03 21:01:36 +0900

    app.js Schema code fail restored

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -9,6 +10,68 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var schema = mongoose.Schema();
+
+var UserSchema = new schema({
+  _id : String,
+    name : String,
+    id : String,
+    password : String,
+    email : String,
+    mail_service : Boolean,
+    favorite : [{
+      type : String,
+        ref : 'stock'
+    }],
+    scrap : [{
+      type : String,
+        ref : 'article'
+    }],
+    register_day : String,
+    current_login : String,
+    point : Number
+});
+
+var StockSchema = new schema({
+  _id : String,
+    code : String,
+    name : String,
+    current_val : String,
+    yesterday_val : String,
+    diff_percentage : String,
+    chat : String
+});
+
+var ArticleSchema = new schema({
+    _id : String,
+    title : String,
+    date : String,
+    content : String,
+    user_cnt : Number,
+    recommend : Number,
+    writer : String
+});
+
+var MessengerSchema = new schema({
+  _id : String,
+    sender : String,
+    recipient : String,
+    date : String,
+    content : String
+});
+
+mongoose.connect("mongodb://localhost:27017/stockchat", function (err) {
+    if(err){
+      console.log("MongoDB Error");
+      throw err;
+    }
+});
+
+var User = mongoose.model('users', UserSchema);
+var Stock = mongoose.model('stock', StockSchema);
+var Article = mongoose.model('article', ArticleSchema);
+var Message = mongoose.model('message', MessengerSchema);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
